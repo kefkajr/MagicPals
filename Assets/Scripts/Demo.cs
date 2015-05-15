6c29bd4af11de0fc4bf85daed842fb43c4dd2063
Jonathan Parham <jonathan.parham@bottlerocketapps.com>
2015-05-15 13:50:34 -0500

    Part 3 - User Input

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour 
+{
+	void OnEnable ()
+	{
+		InputController.moveEvent += OnMoveEvent;
+		InputController.fireEvent += OnFireEvent;
+	}
+	
+	void OnDisable ()
+	{
+		InputController.moveEvent -= OnMoveEvent;
+		InputController.fireEvent -= OnFireEvent;
+	}
+
+	void OnMoveEvent (object sender, InfoEventArgs<Point> e)
+	{
+		Debug.Log("Move " + e.info.ToString());
+	}
+
+	void OnFireEvent (object sender, InfoEventArgs<int> e)
+	{
+		Debug.Log("Fire " + e.info);
+	}
+}
