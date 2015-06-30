8737c62b2a3e41baa982b9851d3db7fbd6f3d584
Jonathan Parham <jonathan.parham@bottlerocketapps.com>
2015-06-30 16:11:17 -0500

    Part 8 - Ability Menu

--- Assets/Scripts/Controller/BattleController.cs
+++ Assets/Scripts/Controller/BattleController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BattleController : StateMachine 
 {
@@ -9,8 +10,10 @@ public class BattleController : StateMachine
 	public LevelData levelData;
 	public Transform tileSelectionIndicator;
 	public Point pos;
-	public Unit currentUnit;
 	public Tile currentTile { get { return board.GetTile(pos); }}
+	public AbilityMenuPanelController abilityMenuPanelController;
+	public Turn turn = new Turn();
+	public List<Unit> units = new List<Unit>();
 
 	void Start ()
 	{
