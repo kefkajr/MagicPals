64b76c7d3552c49a6356b4b71a498f86afd7ef5d
Terry Torres <terry.torres@betterment.com>
2022-06-17 17:04:53 -0400

    Improve camera rotation

--- Assets/Scripts/View Model Component/CameraRig.cs
+++ Assets/Scripts/View Model Component/CameraRig.cs
@@ -8,27 +8,20 @@ public class CameraRig : MonoBehaviour
 
 
 	public float rotateSpeed = 3f;
-	public Transform pitch;
-	Vector3 targetAngles;
+	public Directions currentDirection = Directions.North;
+	TransformLocalEulerTweener rotateTweener;
 
 	bool isRotating = false;
 
 	void Awake ()
 	{
 		AddListeners();
-		targetAngles = pitch.transform.localEulerAngles;
 	}
 	
 	void Update ()
 	{
 		if (follow)
 			transform.position = Vector3.Lerp(transform.position, follow.position, speed * Time.deltaTime);
-
-		//if (pitch.transform.eulerAngles.y != targetAngles.y)
-		//{
-		//	Debug.Log(pitch.transform.eulerAngles.y.ToString() + " " + targetAngles.y.ToString());
-		//	pitch.transform.eulerAngles = Vector3.Lerp(pitch.transform.eulerAngles, targetAngles, rotateSpeed * Time.deltaTime);
-		//}
 	}
 
 	public void Exit()
@@ -53,30 +46,41 @@ public class CameraRig : MonoBehaviour
 
 	protected void TurnCamera(object sender, InfoEventArgs<int> e)
 	{
-		if (e.info == -1)
-			targetAngles.y -= 90;
-		else
-			targetAngles.y += 90;
+		int newDirectionValue = (int)currentDirection + e.info;
 
-		if (targetAngles.y > 360)
-			targetAngles.y -= 360;
-		else if (targetAngles.y < 0)
-			targetAngles.y += 360;
+		if (newDirectionValue < 0)
+			newDirectionValue = 3;
+		if (newDirectionValue > 3)
+			newDirectionValue = 0;
 
-		StartCoroutine(Rotate());
+		currentDirection = (Directions)newDirectionValue;
+
+		StartCoroutine(Turn());
 	}
 
-	// Might be a better way to do this (Rotate instead of RotateToLocal perhaps?), but this works for now!
-	private IEnumerator Rotate()
+	protected virtual IEnumerator Turn()
 	{
-		TransformLocalEulerTweener t = (TransformLocalEulerTweener)pitch.transform.RotateToLocal(targetAngles, 0.25f, EasingEquations.EaseInOutQuad);
+		if (isRotating) yield return null;
 
+		isRotating = true;
+
+		if (rotateTweener != null) rotateTweener.Stop();
+
+		TransformLocalEulerTweener t = (TransformLocalEulerTweener)transform.RotateToLocal(currentDirection.ToEuler(), 0.25f, EasingEquations.EaseInOutQuad);
+
+		// When rotating between North and West, we must make an exception so it looks like the unit
+		// rotates the most efficient way (since 0 and 360 are treated the same)
 		if (Mathf.Approximately(t.startTweenValue.y, 0f) && Mathf.Approximately(t.endTweenValue.y, 270f))
 			t.startTweenValue = new Vector3(t.startTweenValue.x, 360f, t.startTweenValue.z);
 		else if (Mathf.Approximately(t.startTweenValue.y, 270) && Mathf.Approximately(t.endTweenValue.y, 0))
 			t.endTweenValue = new Vector3(t.startTweenValue.x, 360f, t.startTweenValue.z);
+
+		rotateTweener = t;
+
 		while (t != null)
 			yield return null;
+
+		isRotating = false;
 	}
 
 }
