96822e1de04f8f01be7b08c9648b328518524707
Terry Torres <>
2024-05-29 17:01:38 -0400

    Remove redundant Pereception.awarenesses property.

--- Assets/Scripts/View Model Component/Awareness/Perception.cs
+++ Assets/Scripts/View Model Component/Awareness/Perception.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 public class Perception : MonoBehaviour {
 	public Vector2 viewingRange = Vector2.zero; // cone, length and diameter
 	public float hearingRange = 0f; // radius of a circle
-	public HashSet<Awareness> awarenesses = new HashSet<Awareness>(); // list of awarenesses, known stealths and their types
 
 	public Unit unit { get { return GetComponentInParent<Unit>(); } }
 
